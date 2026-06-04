537e0fbe1449fdce1f70693e963dccbbc6e2c3c0
EMANUEL EITAN MIKLASKI <49628417@ort>
2026-06-04 08:51:23 -0300

    avance en js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,13 @@
 ﻿const palabra = document.getElementById("palabra");
 let letra = document.getElementById("letra");
 
+function guionPalabra()
+{
+    for
+}
 function arriesgarLetra()
 {
     const erradas = []
+    
 
 }
