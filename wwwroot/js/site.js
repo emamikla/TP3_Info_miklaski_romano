c6393490d0072423e8ed82ace5c29bf6ae33f59c
EMANUEL EITAN MIKLASKI <49628417@ort>
2026-06-05 09:01:41 -0300

    avances grandes en js. falta que se ejecute bien

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,72 @@
 ﻿const palabra = document.getElementById("palabra");
+const palabraElegida = palabra.value;
 let letra = document.getElementById("letra");
+let listaLetras = palabraElegida.split('');
+let palabraOculta = [];
+let listaErrores = []; 
 
+let guiones= document.getElementById("guionPalabra");
+let resultado = document.getElementById("resultado");
+let intentosRestantes = document.getElementById("intentosRestantes");
+let letrasErradas = document.getElementById("letrasFaltantes");
 
-for (let i = 0 ; i < palabra.length ; i ++ ){
-    console.log("_ ")
+for (let i = 0; i < listaLetras.length; i++){
+    palabraOculta.push ("_");
 }
 
-function arriesgarLetra()
-{
-    const erradas = []
-    
+guiones.innerHTML = palabraOculta.join(' '); 
+intentosRestantes.innerHTML = "Intentos restantes: " + (10 - listaErrores.length);
+letrasErradas.innerHTML = listaErrores.join(', ');
 
+function ArriesgarLetra()
+{
+    while (listaErrores.length < 10 && palabraOculta.includes("_"))
+    {
+        let buscar = buscarLetra(letra.value);
+        
+        if (buscar != null)
+        {
+            for (let i = 0; i < listaLetras.length; i++ )
+            {
+                if (listaLetras[i] === buscar)
+                {
+                    palabraOculta[i] = buscar; 
+                }
+            }
+        }
+        else
+        {
+            listaErrores.push(letra.value); 
+            intentosRestantes.innerHTML = "Intentos restantes: " + (10 - listaErrores.length);
+        }
+
+    }  
+    if (listaLetras.join('') === palabraOculta.join(''))
+    {
+        resultado.innerHTML = "¡Felicidades! Has adivinado la palabra.";
+    }
+    else if (listaErrores.length >= 10)
+    {
+        resultado.innerHTML = "¡Has perdido! La palabra era: " + palabraElegida;
+    }
+
+} 
+
+
+
+function buscarLetra(letra)
+{
+    let devuelvo = "";
+    let i = 0; 
+    while (i< listaLetras.length && listaLetras[i] != letra){
+        i++;
+    }
+    if(i >= listaLetras.length)
+    {
+        devuelvo = null;
+    }
+    else{
+        devuelvo = listaLetras[i];
+    }
+    return devuelvo
 }
