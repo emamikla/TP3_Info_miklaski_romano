04989b651c7c57058b25d62347d692c632de4008
EMANUEL EITAN MIKLASKI <49628417@ort>
2026-06-10 08:58:16 -0300

    todo terminado menos css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,28 +1,34 @@
-﻿const palabra = document.getElementById("palabra");
-const palabraElegida = palabra.value;
-let letra = document.getElementById("letra");
+﻿const palabraElem = document.getElementById("palabra");
+let palabraElegida = document.getElementById("palabra").value;
+const letra = document.getElementById("letra").value.toUpperCase();
 let listaLetras = palabraElegida.split('');
-let palabraOculta = [];
-let listaErrores = []; 
 
-let guiones= document.getElementById("guionPalabra");
-let resultado = document.getElementById("resultado");
-let intentosRestantes = document.getElementById("intentosRestantes");
-let letrasErradas = document.getElementById("letrasFaltantes");
+let palabraOculta = [];
+let listaErrores = [];
+
+const guiones = document.getElementById("guionPalabra");
+const resultado = document.getElementById("resultado");
+const intentosRestantes = document.getElementById("intentosRestantes");
+const letrasErradas = document.getElementById("letrasFaltantes");
 
 for (let i = 0; i < listaLetras.length; i++){
-    palabraOculta.push ("_");
+    palabraOculta[i] = "_";
+}
+ActualizarPantalla();
+
+function ActualizarPantalla() {
+    guiones.innerHTML = palabraOculta.join(' ');
+    intentosRestantes.innerHTML = "Intentos restantes: " + (10 - listaErrores.length);
+    letrasErradas.innerHTML = listaErrores.join(', ');
 }
 
-guiones.innerHTML = palabraOculta.join(' '); 
-intentosRestantes.innerHTML = "Intentos restantes: " + (10 - listaErrores.length);
-letrasErradas.innerHTML = listaErrores.join(', ');
 
 function ArriesgarLetra()
 {
-    while (listaErrores.length < 10 && palabraOculta.includes("_"))
+    const letra = document.getElementById("letra").value.toUpperCase();
+    if (listaErrores.length < 10 && palabraOculta.includes("_"))
     {
-        let buscar = buscarLetra(letra.value);
+        let buscar = buscarLetra(letra);
         
         if (buscar != null)
         {
@@ -33,40 +39,36 @@ function ArriesgarLetra()
                     palabraOculta[i] = buscar; 
                 }
             }
+        } 
+        else {
+            if (!listaErrores.includes(letra)) {
+                listaErrores.push(letra);
+            }
         }
-        else
-        {
-            listaErrores.push(letra.value); 
-            intentosRestantes.innerHTML = "Intentos restantes: " + (10 - listaErrores.length);
+
+        ActualizarPantalla();
+
+        if (listaLetras.join('') === palabraOculta.join('')) {
+            resultado.innerHTML = "¡Felicidades! Has adivinado la palabra.";
+        } else if (listaErrores.length >= 10) {
+            resultado.innerHTML = "¡Has perdido! La palabra era: " + palabraElegida;
         }
+    };
 
-    }  
-    if (listaLetras.join('') === palabraOculta.join(''))
-    {
-        resultado.innerHTML = "¡Felicidades! Has adivinado la palabra.";
+    function buscarLetra(letra) {
+        
+        let i = 0;
+        let l = null ; 
+        while (i < listaLetras.length && listaLetras[i] !== letra){
+            
+            i++;
+        }
+        if (i < listaLetras.length) {
+            l = listaLetras[i];
+        }
+        else {
+            l = null;
+        }
+        return l;
     }
-    else if (listaErrores.length >= 10)
-    {
-        resultado.innerHTML = "¡Has perdido! La palabra era: " + palabraElegida;
-    }
-
-} 
-
-
-
-function buscarLetra(letra)
-{
-    let devuelvo = "";
-    let i = 0; 
-    while (i< listaLetras.length && listaLetras[i] != letra){
-        i++;
-    }
-    if(i >= listaLetras.length)
-    {
-        devuelvo = null;
-    }
-    else{
-        devuelvo = listaLetras[i];
-    }
-    return devuelvo
 }
